de2df35d09565454c8c12a0dd4df8faeda7bbd6f
anushkaaaa1243 <anushkagandhi80@gmail.com>
2024-03-25 01:22:14 +0530

    Variable

---
+++ Variable/variable.js
@@ -0,0 +1,14 @@
+const accountId= 123;
+let accountEmail="Anushkagandhi90@gmail.com";
+var accountPassword="12345";
+accountCity="Bhopal"
+
+// accountId = 567
+ 
+accountEmail ="purvigandhi@gmail.com"
+accountPassword="6789";
+accountCity="Raipur"
+
+console.log(accountId);  
+// console.log([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity])
