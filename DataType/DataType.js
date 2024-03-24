e974a8f990a369057e383e06cef8b8d7ad667aab
anushkaaaa1243 <anushkagandhi80@gmail.com>
2024-03-25 02:11:35 +0530

    Datatype

---
+++ DataType/DataType.js
@@ -0,0 +1,24 @@
+// Primitive datatype
+
+// 1 . Number
+// 2. Symobols
+// 3.  Bollean
+// 4.  String
+// 5.  Bigint
+// 6.   Null
+// 7. Undefined
+
+let name= "Anushka"
+let age =20
+let isLoggedIn=true
+
+
+// Object
+
+console.log(typeof "Anushka")
+console.log(typeof age)
+console.log(typeof null)       // Output is object
+console.log(typeof undefine)   // Output is null
+console.log(typeof isLoggedIn)
+
+
---
+++ DataType/index.js
@@ -0,0 +1,48 @@
+
+ let isNull = null
+ console.log(isNull)
+
+ let isUndefined =undefined
+ console.log(isUndefined)
+
+ let isTrue =true
+ console.log(isTrue)
+
+let score = 77
+console.log(typeof score)
+
+
+let age = "34"
+console.log(typeof age)
+
+
+let valueInage = Number(age)
+console.log(typeof valueInage)
+
+
+
+let number = "32abc"
+let valueInNumber = Number(number)
+console.log(valueInNumber)
+
+
+let isLoggedIn =  1
+let BolleanIsLoggendIn = Boolean (isLoggedIn)
+console.log(BolleanIsLoggendIn)
+
+
+let logged = ""
+let IsLoggendIn = Boolean (logged)
+console.log(IsLoggendIn)
+
+let name = "Anushka"
+let LoggendIn = Boolean (name)
+console.log(LoggendIn)
+
+
+
+let SomeNumber = 33
+
+let stringNumber=String(SomeNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
