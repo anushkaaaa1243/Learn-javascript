289d9d2157d8344ce58f1b97fa669512c3043cad
anushkaaaa1243 <anushkagandhi80@gmail.com>
2024-03-30 17:41:36 +0530

    String,operation,condition

---
+++ Operation/Operation.js
@@ -0,0 +1,27 @@
+// console.log(2+2)
+// console.log(10-5)
+// console.log(5*5)
+// console.log(5/2)
+// console.log(5%5)
+
+// let str1 = "Anushka"
+// let str2 = " Gandhi"
+// let str3=str1+str2
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log("2"+"1")
+// console.log("1"+2 +3)
+// console.log(1+2+"3")
+
+
+// console.log(true)
+
+
+let gameCounter =100;
+gameCounter ++;
+console.log(gameCounter)
+
+
+
---
+++ String/string.js
@@ -0,0 +1,25 @@
+const name="Anushka"
+const age =16
+
+// console.log(`Hello my name is ${name} and my age is ${age}`)
+
+const gameName = new String ("Jalebi")
+
+
+// console.log(gameName)
+// console.log(gameName[1])
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('e'))
+
+const newString =gameName.substring(0,4)
+console.log(newString)
+
+
+const anotherString =gameName.slice(2,4)
+console.log(anotherString)
+
+
+
