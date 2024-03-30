c9e6170fd1b7e099bab505e36b2e6cdf5a331aee
anushkaaaa1243 <anushkagandhi80@gmail.com>
2024-03-31 00:36:52 +0530

    Math and number

---
+++ Maths/math.js
@@ -0,0 +1,15 @@
+// console.log(Math)
+// console.log(Math.abs(-11))
+// console.log(Math.round(44.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(8.9))
+// console.log(Math.min(1,4,6,7,8))
+// console.log(Math.max(1,4,6,7,8))
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min)
---
+++ Number/number.js
@@ -0,0 +1,18 @@
+const score =200
+console.log(score)
+
+const balance =new Number (1000)
+console.log(balance)
+console.log(balance.toString().typeof)
+console.log(typeof balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const otherNumber =234.678
+console.log(otherNumber.toPrecision(3))
+
+const number=100000
+console.log(number.toLocaleString())
+
+
