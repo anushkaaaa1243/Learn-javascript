b5127c2e03761d00d9d2f7266efd2e14d8ef3e4f
anushkaaaa1243 <anushkagandhi80@gmail.com>
2024-04-01 23:55:34 +0530

    Array

---
+++ Array/index.js
@@ -0,0 +1,28 @@
+// Array Method
+
+const Array=[1,2,3,4,5]
+// Array.push(6)
+// Array.pop()
+// Array.unshift(8)
+// Array.shift()
+
+// console.log(Array.includes(7))
+// console.log(Array.indexOf(5))
+// console.log(Array)
+
+const myArray = [1, 2, 3, 4, 5]; 
+
+// const newArray = myArray.join(); 
+// console.log(myArray);
+// console.log(newArray); 
+
+
+// Slice ,splice
+
+console.log("A",myArray)
+const slice= myArray.slice(1,2)
+console.log(slice)
+
+const splice= myArray.splice(1,3)
+console.log("B",myArray)
+console.log(splice)
